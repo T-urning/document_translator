bc1b27525f5d96e4eb2564873e5e4ab623732832
T-urning <xiajing1223@foxmail.com>
2021-05-10 15:51:36 +0800

    Speed up.

--- translate_zh2eng_document.py
+++ translate_zh2eng_document.py
@@ -5,8 +5,12 @@ import yaml
 import random
 import requests
 
+from cProfile import Profile    # 评估性能
+from pstats import Stats
 from hashlib import md5
 from tqdm import tqdm
+from urllib3.util.retry import Retry
+from requests.adapters import HTTPAdapter
 
 # 加载配置文件
 config = yaml.load(open('config.yaml', mode='r', encoding='utf-8'))
@@ -14,6 +18,7 @@ config = yaml.load(open('config.yaml', mode='r', encoding='utf-8'))
 TRANSLATE_API = 'https://fanyi-api.baidu.com/api/trans/vip/translate'
 APP_ID = str(config['API']['appid']) 
 APP_KEY = config['API']['appkey']
+SLEEP_TIME = 1
 
 # Generate salt and sign
 def make_md5(s, encoding='utf-8'):
@@ -26,7 +31,7 @@ def translate(text, source_lang=None, target_lang=None):
     '''
     text = text.strip()
     if not text:
-        raise ValueError('传入的文本不能为空！')
+        return text
     if source_lang is None:
         source_lang = 'zh' # 'auto' -> 自动检测语种
     if target_lang is None:
@@ -37,22 +42,31 @@ def translate(text, source_lang=None, target_lang=None):
     # Build request
     headers = {'Content-Type': 'application/x-www-form-urlencoded'}
     payload = {'appid': APP_ID, 'q': text, 'from': source_lang, 'to': target_lang, 'salt': salt, 'sign': sign}
-
+    
+    s = requests.Session()
+    retries = Retry(total=10)
+    s.mount('https://', HTTPAdapter(max_retries=retries))
     # Send request
-    r = requests.post(TRANSLATE_API, params=payload, headers=headers)
+    r = s.post(TRANSLATE_API, params=payload, headers=headers)
     result = r.json()
     # 拦截错误
     if 'error_code' in result.keys():
         error_code = result['error_code']
         raise Exception(f'调用百度翻译 API 出现错误，错误码为 {error_code}，' +
             '请根据错误码到 https://fanyi-api.baidu.com/product/113 找相应的解决方法。')
+    trans_list = [trans_res['dst'] for trans_res in result['trans_result']]
+    return trans_list
 
-    return result['trans_result'][0]['dst']
+def paras_generator(all_paras, batch_size=5):
+    all_paras_new = [para for para in all_paras if len(para.text.strip()) > 0]
+    total = len(all_paras_new)
+    start = 0
+    for start in range(0, total, batch_size):
+        end = start + batch_size
+        end = total if end > total else end
+        yield all_paras_new[start: end]
 
-
-
-if __name__ == '__main__':
-
+def main():
     # 待翻译文档路径
     file_path = config['FILE']['file_path'] #'test_document.docx'
     
@@ -63,34 +77,66 @@ if __name__ == '__main__':
 
     # 翻译所有段落，包括图片与表格的标题名
     print('处理所有段落，包括图片与表格的标题名...')
-    for para in tqdm(all_paras):
-        if len(para.runs) < 1:
-            continue
-        # 记录该段落的字体样式
-        style = para.runs[0].style 
-        text_zh = para.text.strip()
-        # 对段落整体进行翻译
-        if text_zh:
-            text_en = translate(text_zh)
-            para.text = para.text.replace(text_zh, text_en)
-        # 设置回原先的样式
-        for run in para.runs:
-            run.style = style
-        # 受翻译API访问频率的限制
-        time.sleep(1)
+    batch_size = 10 # 为了提升翻译速度，每次传入 batch_size 个段落同时翻译
+    for para_batch in tqdm(paras_generator(all_paras, batch_size)):
+        # paras_generator 确保了选取每个 para 的文本都不为空
+        para_combined = '\n'.join([para.text.strip() for para in para_batch])
+        try:
+            para_translated = translate(para_combined)
+        except Exception as e:
+            print('翻译失败')
+            raise e
+        else:   # 翻译成功则执行：
+            assert len(para_batch) == len(para_translated)
+            for para, text_target in zip(para_batch, para_translated):
+                text_source = para.text.strip()
+                if len(para.runs) < 1:
+                    continue
+                # 记录该段落的字体样式
+                style = para.runs[0].style 
+                para.text = para.text.replace(text_source, text_target)
+                # 设置回原先的样式
+                for run in para.runs:
+                    run.style = style
+            # 受翻译API访问频率的限制
+            time.sleep(SLEEP_TIME)
     
     # 翻译表格内容
     print('处理所有表格...')
     for table in tqdm(all_tables): # 所有表格
-        for i, row in tqdm(enumerate(table.rows)): # 表格的所有行
-            for j, cell in enumerate(row.cells): # 该行的所有元素
-                text_zh = cell.text.strip()
-                if text_zh:
-                    text_en = translate(text_zh)
-                    cell.text = cell.text.replace(text_zh, text_en)
-                    time.sleep(1)
+        table_values = []
+        cells_to_translate = []
+        for row in table.rows: # 表格的所有行
+            for cell in row.cells: # 该行的所有元素
+                text_source = cell.text.strip()
+                if len(text_source) > 0:
+                    table_values.append(text_source)
+                    cells_to_translate.append(cell)
+        if table_values:
+            table_values = '\n'.join(table_values)
+            try:
+                table_translated = translate(table_values)
+            except Exception as e:
+                print('翻译出错')
+                raise e
+            else: # 翻译成功
+                assert len(cells_to_translate) == len(table_translated)
+                for cell, text_target in zip(cells_to_translate, table_translated):
+                    text_source = cell.text.strip()
+                    cell.text = cell.text.replace(text_source, text_target)
+                time.sleep(SLEEP_TIME)
     # 保存翻译结果              
     save_path = config['FILE']['save_path']             
     doc.save(save_path)
     print('翻译结果已保存。')
+
+if __name__ == '__main__':
+    # profiler = Profile()
+    # profiler.runcall(main)
+    # stats = Stats(profiler)
+    # stats.sort_stats('cumulative')
+    # stats.print_stats(50)
+    main()
+
+    
 
